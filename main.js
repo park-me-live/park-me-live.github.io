348b6f0259accfbab977ff0ea057ac05127f8546
Oleg Vasilyev <this.olegvasilyev@gmail.com>
2019-06-09 22:59:28 +0300

    height fix

--- main.js
+++ main.js
@@ -17,4 +17,12 @@
       inline: "nearest"
     });
   });
+
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+  window.addEventListener("resize", () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  });
 })();
