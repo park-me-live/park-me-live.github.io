170fddab74b3a8a031f8b2d5ae9789c42b594bc9
Andrei Kurylau <andrei_kurylau@outlook.com>
2019-06-10 14:32:23 +0300

    trying height fix 3

--- main.js
+++ main.js
@@ -18,23 +18,28 @@
     });
   });
 
-/*   const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
-
-  window.addEventListener("resize", () => {
-    const vh = window.innerHeight * 0.01;
+  /*   const vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
-  }); */
+  
+    window.addEventListener("resize", () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    }); */
+
+  window.onresize = function () {
+    document.documentElement.style.setProperty("--sectionHeight", `${window.innerHeight}px`);
+  }
+  window.onresize();
 
   const subscribeBtn = document.querySelector("#subscribe-button");
   subscribeBtn.addEventListener("click", _ => {
     const emailInputRef = document.querySelector("#email-input");
     Email.send({
-      SecureToken : "23c165e4-8d22-4f33-b5f3-8719281171df",
-      To : 'parkmelive@gmail.com',
-      From : "parkmelive@gmail.com",
-      Subject : "New Subscriber",
-      Body : emailInputRef.value
+      SecureToken: "23c165e4-8d22-4f33-b5f3-8719281171df",
+      To: 'parkmelive@gmail.com',
+      From: "parkmelive@gmail.com",
+      Subject: "New Subscriber",
+      Body: emailInputRef.value
     }).then(() => {
       console.log("success");
     });
