35f5afc7943682ff02fd5f49e27c44b8c2ed0519
Andrei Kurylau <andrei_kurylau@outlook.com>
2019-06-10 13:25:36 +0300

    Fixed Fonts + Added email sending

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-(function() {
+(function () {
   const loadingRef = document.querySelector("#loading");
   setTimeout(_ => {
     loadingRef.style.opacity = 0;
     setTimeout(_ => {
       loadingRef.style.display = "none";
-    }, 1000);
-  }, 500);
+    }, 2000);
+  }, 2000);
 
   const becomeFirstRef = document.querySelector("#become-first");
   becomeFirstRef.addEventListener("click", _ => {
@@ -24,5 +24,19 @@
   window.addEventListener("resize", () => {
     const vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
+  });
+
+  const subscribeBtn = document.querySelector("#subscribe-button");
+  subscribeBtn.addEventListener("click", _ => {
+    const emailInputRef = document.querySelector("#email-input");
+    Email.send({
+      SecureToken : "23c165e4-8d22-4f33-b5f3-8719281171df",
+      To : 'parkmelive@gmail.com',
+      From : "parkmelive@gmail.com",
+      Subject : "New Subscriber",
+      Body : emailInputRef.value
+    }).then(() => {
+      console.log("success");
+    });
   });
 })();
