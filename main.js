6230cccb7b046770e392874ba4fe95ab6a0f9976
Oleg Vasilyev <this.olegvasilyev@gmail.com>
2019-06-11 16:21:33 +0300

    scroll disabled

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
 (function () {
-  this.oldScroll = 0;
-  this.oldHeight = window.innerHeight;
-  this.scrollInterval;
-
   // Handling loading screen
   const loadingRef = document.querySelector("#loading");
   setTimeout(_ => {
@@ -25,73 +21,18 @@
       block: "end",
       inline: "nearest"
     });
-
-    this.oldHeight = window.innerHeight;
-    this.oldScroll = document.body.scrollHeight - this.oldHeight;
-
-    clearInterval(this.scrollInterval);
-    this.scrollInterval = setInterval(() => {
-      if (document.body.scrollTop === this.oldScroll) {
-        clearInterval(this.scrollInterval);
-      }
-    }, 100);
   });
 
   // Handling scroll behaviour on mobile devices
   if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
     document.documentElement.style.setProperty("--sectionHeight", `${window.innerHeight}px`);
 
-    document.body.ontouchend = () => {
-      if (window.innerHeight < 600) return;
-
+    window.addEventListener('resize', _ => {
       document.documentElement.style.setProperty("--sectionHeight", `${window.innerHeight}px`);
-
-      const currentScroll = document.body.scrollTop;
-
-      if (Math.abs(currentScroll - this.oldScroll) < 25) {
-        this.oldScroll = (this.oldScroll / this.oldHeight) * window.innerHeight;
-        this.oldHeight = window.innerHeight;
-
-        window.scrollTo(0, this.oldScroll);
-        return;
-      }
-
-      const direction = currentScroll < this.oldScroll ? -1 : 1;
-      let currentScreen = Math.round((this.oldScroll + direction * this.oldHeight) / this.oldHeight);
-      currentScreen = currentScreen > 0 ? currentScreen : 0;
-
-      this.isScrolling = true;
-      window.scrollTo({
-        top: window.innerHeight * currentScreen,
-        behavior: "smooth"
-      });
-
-      this.oldScroll = window.innerHeight * currentScreen;
-      this.oldHeight = window.innerHeight;
-
-      clearInterval(this.scrollInterval);
-
-      let count = 0;
-      this.scrollInterval = setInterval(() => {
-        count += 50;
-        if ((document.body.scrollTop === this.oldScroll) || count > 1000) {
-          if ((this.oldHeight !== window.innerHeight) && (window.innerHeight >= 600)) {
-            document.documentElement.style.setProperty("--sectionHeight", `${window.innerHeight}px`);
-            this.oldScroll = (this.oldScroll / this.oldHeight) * window.innerHeight;
-            this.oldHeight = window.innerHeight;
-
-            window.scrollTo(0, this.oldScroll);
-          }
-          clearInterval(this.scrollInterval);
-        }
-      }, 50);
-
-    };
-
+    });
   } else {
     document.documentElement.style.setProperty("--sectionHeight", `100%`);
   }
-
 
   // Handling subscribe button
   const subscribeBtn = document.querySelector("#subscribe-button");
