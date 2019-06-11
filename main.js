494eed29862897831259ec1cbc68464c827b034d
Andrei Kurylau <andrei_kurylau@outlook.com>
2019-06-11 13:34:08 +0300

    Reworked section scrolling

--- main.js
+++ main.js
@@ -1,54 +1,74 @@
 (function () {
+  this.isScrolling = false;
+  this.oldScroll = 0;
+  this.oldHeight = window.innerHeight;
+
+  // Handling loading screen
   const loadingRef = document.querySelector("#loading");
   setTimeout(_ => {
+    window.scrollTo(0, 0);
     loadingRef.style.opacity = 0;
     setTimeout(_ => {
       loadingRef.style.display = "none";
     }, 2000);
   }, 2000);
 
+
+  // Handling "Become First" button
   const becomeFirstRef = document.querySelector("#become-first");
   becomeFirstRef.addEventListener("click", _ => {
     const subscribeScreenRef = document.querySelector("#subscribe-screen");
 
+    this.isScrolling = true;
     subscribeScreenRef.scrollIntoView({
       behavior: "smooth",
       block: "end",
       inline: "nearest"
     });
+
+    setTimeout(() => {        
+      this.isScrolling = false;
+    }, 1000);
   });
 
+  // Handling scroll behaviour on mobile devices
   if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
     document.documentElement.style.setProperty("--sectionHeight", `${window.innerHeight}px`);
 
-    let isScrolling;
-    let scrollTimeout
-
     window.addEventListener('scroll', function () {
-      if (isScrolling) return;
+      if (this.isScrolling) return;
 
-      window.clearTimeout(scrollTimeout);
+      document.documentElement.style.setProperty("--sectionHeight", `${window.innerHeight}px`);
 
-      scrollTimeout = setTimeout(function () {
-        document.documentElement.style.setProperty("--sectionHeight", `${window.innerHeight}px`);
+      if (window.innerHeight < 600) return;
 
-        if (window.innerHeight < 600) return;
-        const currentScrollPercent = (document.body.scrollTop / document.body.scrollHeight);
-        const currentScreen = currentScrollPercent < 0.17 ? 0 : (currentScrollPercent < 0.51 ? 1 : 2);
+      const currentScroll = document.body.scrollTop;
+      const direction = currentScroll < this.oldScroll ? -1 : 1;
+      let currentScreen = Math.round((this.oldScroll + direction * this.oldHeight) / this.oldHeight);
+      currentScreen = currentScreen > 0 ? currentScreen : 0;
 
-        this.isScrolling = true;
-        window.scrollTo({
-          top: window.innerHeight * currentScreen,
-          behavior: "smooth"
-        });
+      this.isScrolling = true;
+
+      window.scrollTo({
+        top: window.innerHeight * currentScreen,
+        behavior: "smooth"
+      });
+
+      this.oldScroll = window.innerHeight * currentScreen;
+      this.oldHeight = window.innerHeight;
+
+      setTimeout(() => {        
         this.isScrolling = false;
-      }, 100);
+      }, 500);
+
     }, false);
 
   } else {
     document.documentElement.style.setProperty("--sectionHeight", `100%`);
   }
 
+
+  // Handling subscribe button
   const subscribeBtn = document.querySelector("#subscribe-button");
   subscribeBtn.addEventListener("click", _ => {
     const emailInputRef = document.querySelector("#email-input");
